764ebed885173ac3a083839af84f64bda5e62f8f
paimdev <bathkelucas@gmail.com>
2022-02-16 21:58:40 -0300

    Format all files and add gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import logoReact from '../assets/OYIaJ1KK.png'
-import logoGraph from '../assets/12972006.png'
-import logoPostgre from '../assets/ASOhU5xJ.png'
-import logoTS from '../assets/bynNY5dJ.jpg'
-import logoPHP from '../assets/hwUcGZ41_400x400.jpg'
-import logoJava from '../assets/K85ZWV2F.png'
-import logoNode from '../assets/n1JRsFeB_400x400.png'
-import logoNext from '../assets/nextjs.png'
-import logoGo from '../assets/O6AczwfV_400x400.png'
-import logoPython from '../assets/pUBY5pVj.png'
-import logoRedis from '../assets/redis.png'
-import logoAngular from '../assets/square.png'
+import logoReact from "../assets/OYIaJ1KK.png";
+import logoGraph from "../assets/12972006.png";
+import logoPostgre from "../assets/ASOhU5xJ.png";
+import logoTS from "../assets/bynNY5dJ.jpg";
+import logoPHP from "../assets/hwUcGZ41_400x400.jpg";
+import logoJava from "../assets/K85ZWV2F.png";
+import logoNode from "../assets/n1JRsFeB_400x400.png";
+import logoNext from "../assets/nextjs.png";
+import logoGo from "../assets/O6AczwfV_400x400.png";
+import logoPython from "../assets/pUBY5pVj.png";
+import logoRedis from "../assets/redis.png";
+import logoAngular from "../assets/square.png";
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import Scoreboard from "./Scoreboard";
@@ -24,7 +24,7 @@ const App = () => {
     card3: false,
     card4: false,
     card5: false,
-    card6: false, 
+    card6: false,
     card7: false,
     card8: false,
     card9: false,
@@ -56,7 +56,7 @@ const App = () => {
       card3: false,
       card4: false,
       card5: false,
-      card6: false, 
+      card6: false,
       card7: false,
       card8: false,
       card9: false,
@@ -71,7 +71,7 @@ const App = () => {
       setBestScore(currentScore);
     }
   };
-  
+
   const cardsArray = [
     <Card name="card1" handleClick={handleClick} src={logoReact} key="1" />,
     <Card name="card2" handleClick={handleClick} src={logoGraph} key="2" />,
@@ -84,8 +84,8 @@ const App = () => {
     <Card name="card9" handleClick={handleClick} src={logoGo} key="9" />,
     <Card name="card10" handleClick={handleClick} src={logoPython} key="10" />,
     <Card name="card11" handleClick={handleClick} src={logoRedis} key="11" />,
-    <Card name="card12" handleClick={handleClick} src={logoAngular} key="12" />
-    ].sort(() => Math.random() - 0.5)
+    <Card name="card12" handleClick={handleClick} src={logoAngular} key="12" />,
+  ].sort(() => Math.random() - 0.5);
 
   return (
     <div className="flex flex-col bg-slate-100">
@@ -93,10 +93,10 @@ const App = () => {
         <h1 className="text-3xl text-black">Stack Memory Card Game</h1>
         <Scoreboard currentScore={currentScore} bestScore={bestScore} />
       </div>
-      <div className='flex flex-col items-center'>
-      <div className="lg:grid lg:grid-cols-4 lg:justify-items-center lg:w-3/4">
-        {cardsArray}
-      </div>
+      <div className="flex flex-col items-center">
+        <div className="lg:grid lg:grid-cols-4 lg:justify-items-center lg:w-3/4">
+          {cardsArray}
+        </div>
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 function Card(props) {
-
   return (
-    <div className="card border border-black m-4 w-48"
-    name={props.name} onClick={props.handleClick}
+    <div
+      className="card border border-black m-4 w-48"
+      name={props.name}
+      onClick={props.handleClick}
     >
-    <img name={props.name} src={props.src} alt={props.name} />
+      <img name={props.name} src={props.src} alt={props.name} />
     </div>
   );
 }
